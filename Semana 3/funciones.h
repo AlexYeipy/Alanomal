8a645770be79f423240215a24479462ccee654af
AlexYeipy <alexgp100@gmail.com>
2025-12-02 14:18:54 +0100

    Update funciones.h

--- Semana 3/funciones.h
+++ Semana 3/funciones.h
@@ -9,4 +9,8 @@ using namespace std;
 
 Player parseInitialMessage(std::string &message, Player &player);
 
+vector<string> separate_string(string & s);
+
+vector<string> separate_string_separator(string & s, string separator);
+
 void sendInitialMoveMessage(const Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient);
