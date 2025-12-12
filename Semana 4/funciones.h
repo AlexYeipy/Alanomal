84cc81569f9e562b0524b1a9b942a124efbd68ab
andrespicazo <101358058+andrespicazo@users.noreply.github.com>
2025-12-12 09:17:38 +0100

    Add new functions for message handling and kickoff

--- Semana 4/funciones.h
+++ Semana 4/funciones.h
@@ -15,4 +15,9 @@ vector<string> separate_string_separator(string & s, string separator);
 
 void sendInitialMoveMessage(Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient);
 
+vector<string> parse_message_hear(const std::string &s);
+
+void sendInitialKickOffMessage(Player &player, MinimalSocket::udp::Udp<true> &udp_socket, MinimalSocket::Address const &recipient);
+
 optional<GoalInfo> parseGoalOpponent(const std::string& seeMsg, const std::string& mySide);
+
